722927c341b8f877a246c44d5f0416a5bf3eb35a
firdausibnu <firdausibnu@hotmail.com>
2015-10-04 22:15:24 +0700

    updates

--- templates/attachments.php
+++ templates/attachments.php
@@ -4,17 +4,18 @@ function saveAttachments() {
     global $app;
 
     $req = $app->request->post();
-
-    echo count($_FILES);
-
-//    echo count($_FILES);
     
-    $destination = 'attachments/' . $filename;
-    move_uploaded_file($_FILES['file']['tmp_name'], $destination);
+    echo $is_uploaded = $req['is_uploaded'];
+
+    print_r($_FILES);
+    
+
+    $destination = 'assets/uploaded/' . $_FILES['isi']['name'];
+    move_uploaded_file($_FILES['isi']['tmp_name'], $destination);
 
 //    $req = json_decode($app->request->getBody(), true);
 //
-    $token = $req['token'];
+//    $token = $req['token'];
 //
 //    $decode = JWT::decode($token, TK);
 //
--- templates/submitSurat.php
+++ templates/submitSurat.php
@@ -8,14 +8,14 @@ function submitSurat() {
     $req = $app->request->post();
 
     $paramToken = $req['token'];
-
+    $paramUploaded = $req['is_uploaded'];
     $decode = JWT::decode($paramToken, TK);
     if ($decode->valid) {
         $paramSubject = $req['subject'];
         $paramIdInstitusi = $decode->id_institusi;
         $paramNamaInstitusi = $decode->nama_institusi;
         $paramHal = $req['hal'];
-        $paramLampiran = count($_FILES);
+        $paramLampiran = $req['lampiran'];
 
         $paramTujuan = json_decode($req['tujuan']);
         $tujuan = "";
@@ -29,7 +29,12 @@ function submitSurat() {
             $penandatangan = $paramPenandatangan[0]->identifier;
         }
 
-        $paramIsi = str_replace('<span style="color: rgba(0, 0, 0, 0.870588);float: none;background-color: rgb(255, 255, 255);">', '', $req['isi']);
+        if (isset($req['isi'])) {
+            $paramIsi = str_replace('<span style="color: rgba(0, 0, 0, 0.870588);float: none;background-color: rgb(255, 255, 255);">', '', $req['isi']);
+        } else {
+            $paramIsi = "";
+        }
+
         $paramTanggalSurat = $req['tanggal_surat'];
 
         $timezone_identifier = "Asia/Jakarta";
@@ -38,7 +43,7 @@ function submitSurat() {
 
         $nosurat = checkCounter($db, $paramIdInstitusi, false) . "/UN39." . getKodeUnit($db, $paramIdInstitusi) . "/" . $paramHal . "/" . date('y');
 
-        $query = "INSERT INTO `surat`(subject_surat, tujuan, kode_lembaga_pengirim, penandatangan, no_surat, lampiran, kode_hal, isi, tembusan, tanggal_surat) VALUES(:subject_surat, :tujuan, :id_institusi, :penandatangan, :nosurat, :lampiran, :hal, :isi, :tembusan, :tanggal_surat)";
+        $query = "INSERT INTO `surat`(subject_surat, tujuan, kode_lembaga_pengirim, penandatangan, no_surat, lampiran, kode_hal, isi, tembusan, tanggal_surat, is_uploaded) VALUES(:subject_surat, :tujuan, :id_institusi, :penandatangan, :nosurat, :lampiran, :hal, :isi, :tembusan, :tanggal_surat, :is_uploaded)";
 
         $stmt = $db->prepare($query);
         $stmt->bindValue(":subject_surat", $paramSubject);
@@ -49,13 +54,14 @@ function submitSurat() {
         $stmt->bindValue(":lampiran", (int) $paramLampiran, PDO::PARAM_INT);
         $stmt->bindValue(":hal", $paramHal);
         $stmt->bindValue(":isi", $paramIsi);
+        $stmt->bindValue(":is_uploaded", $paramUploaded);
         $paramTembusan = json_decode($req['tembusan']);
         if ($paramTembusan != null) {
             $tembusan = "";
             for ($i = 0; $i < (count($paramTembusan) - 1); $i++) {
-                $tembusan .= $paramTembusan[$i]['identifier'] . "@+id/";
+                $tembusan .= $paramTembusan[$i]->identifier . "@+id/";
             }
-            $tembusan .= $paramTembusan[$i]['identifier'];
+            $tembusan .= $paramTembusan[$i]->identifier;
             $stmt->bindValue(":tembusan", $tembusan);
         } else {
             $stmt->bindValue(":tembusan", "");
@@ -63,11 +69,26 @@ function submitSurat() {
         $stmt->bindValue(":tanggal_surat", $tanggal_surat);
         try {
             if ($stmt->execute()) {
+
+                if ($paramUploaded == 'true') {
+                    $file_path = 'assets/uploaded/' . $_FILES['isi']['name'];
+                    if (move_uploaded_file($_FILES['isi']['tmp_name'], $file_path)) {
+                        if (InsertSuratUploaded($db, $nosurat, $file_path)) {
+                            if (!InsertSuratAttachment($db, $nosurat, $file_path)) {
+                                die('{"result": "Gagal mengupload lampiran"}');
+                            };
+                        } else {
+                            die('{"result": "Gagal mengupload surat"}');
+                        }
+                    };
+                }
+
                 // Setelah berhasil mengeksekusi query, upload file ke folder yang telah ditentukan
                 for ($i = 0; $i < $paramLampiran; $i++) {
-                    $destination = 'assets/' . $_FILES[$i]['name'];
+                    $destination = 'assets/attachments/' . $_FILES[$i]['name'];
                     move_uploaded_file($_FILES[$i]['tmp_name'], $destination);
                 }
+
                 $registration_ids = array();
                 if ((pushNotification($db, $penandatangan)) != null) {
                     $registration_ids = pushNotification($db, $penandatangan);
@@ -78,14 +99,44 @@ function submitSurat() {
                 $result = $gcm->send_notification($registration_ids, $pesan);
                 echo '{"result": "success"}';
             } else {
-                echo '{"result": "error"}';
+                echo '{"result": "Gagal mengeksekusi query"}';
             }
         } catch (PDOException $ex) {
 //            echo $ex->getMessage();
-            echo '{"result": "error"}';
+            echo '{"result": "' . $ex->getMessage() . '"}';
         }
     } else {
-        echo '{"result": "error"}';
+        echo '{"result": "Token tidak valid"}';
     }
     $db = null;
+}
+
+function InsertSuratUploaded($db, $nosurat, $file_path) {
+    $query = "INSERT INTO `surat_uploaded`(no_surat, file_path) VALUES(:no_surat, :file_path)";
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":no_surat", $nosurat);
+    $stmt->bindValue(":file_path", $file_path);
+    try {
+        if ($stmt->execute()) {
+            return true;
+        }
+    } catch (PDOException $ex) {
+        return false;
+    }
+}
+
+function InsertSuratAttachment($db, $nosurat, $file_path) {
+    $query = "INSERT INTO `surat_lampiran`(no_surat, file_path) VALUES(:no_surat, :file_path)";
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":no_surat", $nosurat);
+    $stmt->bindValue(":file_path", $file_path);
+    try {
+        if ($stmt->execute()) {
+            return true;
+        }
+    } catch (PDOException $ex) {
+        return false;
+    }
 }
